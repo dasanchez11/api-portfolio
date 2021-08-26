8f6810bd2e53b2c1a1e57e696a0dc6ca93aa48a6
Diego Sanchez <diegosanchez@Diegos-MacBook-Pro.local>
2021-08-26 14:32:15 -0700

    mobile fetch

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // ALLOW CORS 
-// app.use((req,res,next)=>{
-//     res.setHeader('Access-Control-Allow-Origin', "http://localhost:3000")
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     next()
-// })
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin', "https://zealous-tesla-e014fe.netlify.app")
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next()
+})
 
 const devProjectRoutes = require('./routes/devProject.route')
 app.use('/devProject', devProjectRoutes)
