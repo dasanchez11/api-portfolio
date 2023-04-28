d54ebcb37c7459d88b72fad5595bcfb427456eaf
dasanchez11 <diegosanchez@Diegos-MacBook-Pro.local>
2023-04-28 01:33:29 -0500

    cors

--- app.js
+++ app.js
@@ -10,13 +10,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+// res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 // ALLOW CORS
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://zealous-tesla-e014fe.netlify.app"
+    "https://zealous-tesla-e014fe.netlify.app/"
   );
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
